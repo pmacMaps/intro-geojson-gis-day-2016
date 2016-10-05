841824d87a5d9c34a7589b333eb2a6b1740ad83d
Patrick McKinney <pnmcartography@gmail.com>
2016-10-05 12:00:35 -0400

    style harrisburg sites and add popup

--- assets/js/mapApp.js
+++ assets/js/mapApp.js
@@ -2,19 +2,30 @@
 
 /*** Variables ***/
 var map;
-var homeCoords =[40.28325793613044, -76.89185142517091];
-var zoomHome;
+var homeCoords =[40.28208, -76.89215];
+var initZoom = 15;
+var zoomHomeControl;
 var mapboxEmerald;
-var midtownSitesParks;
+var harrisburgSitesParks;
+var sitesIcon;
 var mapboxKey = 'pk.eyJ1IjoicG1ja2lubmV5IiwiYSI6ImNpa3NpNTlyNDBlcG51cm0xcG9kd3Z2ZGoifQ.9mtNv6FNVl8c1bd7Kqud_Q';
 var geoJsonUrl = '//raw.githubusercontent.com/pmacMaps/intro-geojson-gis-day-2016/master/midtownSitesParks.geojson';
 
 
 /*** Map & Controls ***/
-var map = L.map('map', {
+map = L.map('map', {
     center: homeCoords,
-    zoom: 14
+    zoom: initZoom,
+    zoomControl: false
 });
+
+// Zoom Home Control
+zoomHomeControl = L.Control.zoomHome({
+    position: 'topleft',
+    zoomHomeTitle: 'Full map extent',
+    homeCoordinates: homeCoords,
+    homeZoom: initZoom
+}).addTo(map);
 
 /*** Layers ***/
 // Mapbox Emerald
@@ -25,7 +36,52 @@ mapboxEmerald = L.tileLayer('//api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?acce
 	accessToken: mapboxKey
 }).addTo(map);
 
-midtownSitesParks = new L.GeoJSON.AJAX(geoJsonUrl).addTo(map);
+// Icon for Sites
+sitesIcon = L.icon({
+    //iconUrl: '../../assets/media/store-flaticon.png',
+    iconUrl: '../../assets/media/fa-check-square-orange.png',
+    iconSize: [25,25]
+});
 
+// Sites and Parks
+harrisburgSitesParks = new L.GeoJSON.AJAX(geoJsonUrl, {
+    // Style point features
+    pointToLayer: function(geoJsonPoint, latlng) {
+       return L.marker(latlng, {
+           icon: sitesIcon,
+           //alt: 'artist rendering of storefront'
+           alt: 'square with a check mark in the center'
+       });
+    },
+    style: function(geoJsonFeature, feature) {
+        return {
+           weight: 0,
+           fillColor: '#A382F1',
+           fillOpacity: 0.5           
+        };
+    }
+}).addTo(map);
 
+// Add Tooltip to Sites and Parks
 
+// Add popup to Sites and Parks
+harrisburgSitesParks.bindPopup(function(evt) {
+    // popup for Sites
+    if (evt.feature.properties.group === 'sites') {
+        var popupContentSites = '<div class="feat-popup">';
+        popupContentSites += '<h3>{name}</h3><hr />';
+        popupContentSites += '<p>This fine establishment is located at {address}.</p>';
+        popupContentSites += '<p>You can visit their <a href="{website}" target="_blank">website</a> for more information.</p>';
+        popupContentSites += '</div>';
+        
+        return L.Util.template(popupContentSites, evt.feature.properties);
+    } // popup for Parks    
+      else if (evt.feature.properties.group === 'parks') {
+        var popupContentParks = '<div class="feat-popup">';
+        popupContentParks += '<h3>{name}</h3><hr />';
+        popupContentParks += '<p>This park is located at {address}, in the {neighborhood} neighborhood.</p>';
+        popupContentParks += '</div>';
+        
+        return L.Util.template(popupContentParks, evt.feature.properties);
+    }
+});
